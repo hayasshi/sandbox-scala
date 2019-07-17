d5a2297efe461d430e2df80d47dd6480cf6d0716
hayasshi <daisuke0884@gmail.com>
2019-07-17 23:38:39 +0900

    Remove smoke test

--- algorithm/src/test/scala/trees/BinaryTreeTest.scala
+++ algorithm/src/test/scala/trees/BinaryTreeTest.scala
@@ -5,16 +5,6 @@ import org.scalatest.DiagrammedAssertions
 
 class BinaryTreeTest extends FunSuite with DiagrammedAssertions {
 
-  test("smoke") {
-    val l = BinaryTree(2, None, None)
-    val r = BinaryTree(3, None, None)
-    val tree = BinaryTree(1, Some(l), Some(r))
-    
-    assert(tree.data == 1)
-    assert(tree.left.get == l)
-    assert(tree.right.get == r)
-  }
-
   /**
    *       4
    *      / \
