da9037047ecca820f3b6f28e808e09b88397f3fd
hayasshi <daisuke0884@gmail.com>
2019-07-18 00:11:09 +0900

    Changed to return BinaryTree's complexity

--- algorithm/src/main/scala/trees/BinaryTree.scala
+++ algorithm/src/main/scala/trees/BinaryTree.scala
@@ -7,49 +7,62 @@ final case class BinaryTree[A](data: A, left: Option[BinaryTree[A]], right: Opti
 
 object BinaryTree {
 
+  sealed abstract class SearchResult[+A]
+  final case class Found[A](value: A, order: Int) extends SearchResult[A]
+  final case class NotFound(order: Int) extends SearchResult[Nothing]
+
   /**
     * 幅優先探索
     */
-  def breadthFirstSearch[A](root: BinaryTree[A])(target: A): Option[A] = {
+  def breadthFirstSearch[A](root: BinaryTree[A])(target: A): SearchResult[A] = {
     // for performance
     val queue = mutable.Queue.empty[BinaryTree[A]]
 
     @tailrec
-    def loop(): Option[A] = {
-      if (queue.isEmpty) None
+    def loop(order: Int): SearchResult[A] = {
+      if (queue.isEmpty) NotFound(order)
       else {
+        val current = order + 1
         val elem = queue.dequeue()
-        if (elem.data == target) Some(elem.data)
+        if (elem.data == target) Found(elem.data, current)
         else {
           elem.left.foreach(data  => queue.enqueue(data))
           elem.right.foreach(data => queue.enqueue(data))
-          loop()
+          loop(current)
         }
       }
     }
 
     queue.enqueue(root)
-    loop()
+    loop(0)
   }
 
   /**
     * 深さ優先探索
     */
-  def depthFirstSearch[A](root: BinaryTree[A])(target: A): Option[A] = root match {
-    case BinaryTree(data, None, None) =>
-      if (data == target) Some(data) else None
-    case BinaryTree(data, Some(lv), None) =>
-      if (data == target) Some(data) else depthFirstSearch(lv)(target)
-    case BinaryTree(data, None, Some(rv)) =>
-      if (data == target) Some(data) else depthFirstSearch(rv)(target)
-    case BinaryTree(data, Some(lv), Some(rv)) =>
-      if (data == target) Some(data)
-      else {
-        depthFirstSearch(lv)(target) match {
-          case None => depthFirstSearch(rv)(target)
-          case v    => v
-        }
+  def depthFirstSearch[A](root: BinaryTree[A])(target: A): SearchResult[A] = {
+
+    def loop(t: BinaryTree[A], order: Int): SearchResult[A] = {
+      val current = order + 1
+      t match {
+        case BinaryTree(data, None, None) =>
+          if (data == target) Found(data, current) else NotFound(current)
+        case BinaryTree(data, Some(lv), None) =>
+          if (data == target) Found(data, current) else loop(lv, current)
+        case BinaryTree(data, None, Some(rv)) =>
+          if (data == target) Found(data, current) else loop(rv, current)
+        case BinaryTree(data, Some(lv), Some(rv)) =>
+          if (data == target) Found(data, current)
+          else {
+            loop(lv, current) match {
+              case NotFound(next) => loop(rv, next)
+              case v    => v
+            }
+          }
       }
+    }
+
+    loop(root, 0)
   }
 
 }
--- algorithm/src/test/scala/trees/BinaryTreeTest.scala
+++ algorithm/src/test/scala/trees/BinaryTreeTest.scala
@@ -2,6 +2,8 @@ package trees
 
 import org.scalatest.FunSuite
 import org.scalatest.DiagrammedAssertions
+import trees.BinaryTree.Found
+import trees.BinaryTree.NotFound
 
 class BinaryTreeTest extends FunSuite with DiagrammedAssertions {
 
@@ -19,30 +21,30 @@ class BinaryTreeTest extends FunSuite with DiagrammedAssertions {
   val _4 = BinaryTree(4, Some(_2), Some(_3))
   val root = _4
 
-  test("bfs - 検索対象が含まれていれば値を返す") {
-    assert(BinaryTree.breadthFirstSearch(root)(4) == Some(4))
-    assert(BinaryTree.breadthFirstSearch(root)(2) == Some(2))
-    assert(BinaryTree.breadthFirstSearch(root)(3) == Some(3))
-    assert(BinaryTree.breadthFirstSearch(root)(5) == Some(5))
-    assert(BinaryTree.breadthFirstSearch(root)(1) == Some(1))
+  test("bfs - 検索対象が含まれていれば値と検索計算量を返す") {
+    assert(BinaryTree.breadthFirstSearch(root)(4) == Found(4, 1))
+    assert(BinaryTree.breadthFirstSearch(root)(2) == Found(2, 2))
+    assert(BinaryTree.breadthFirstSearch(root)(3) == Found(3, 3))
+    assert(BinaryTree.breadthFirstSearch(root)(5) == Found(5, 4))
+    assert(BinaryTree.breadthFirstSearch(root)(1) == Found(1, 5))
   }
 
-  test("bfs - 検索対象が含まれていなければ値を返さない") {
-    assert(BinaryTree.breadthFirstSearch(root)(6) == None)
-    assert(BinaryTree.breadthFirstSearch(root)(0) == None)
+  test("bfs - 検索対象が含まれていなければ全検索計算量のみを返す") {
+    assert(BinaryTree.breadthFirstSearch(root)(6) == NotFound(5))
+    assert(BinaryTree.breadthFirstSearch(root)(0) == NotFound(5))
   }
 
-  test("dfs - 検索対象が含まれていれば値を返す") {
-    assert(BinaryTree.depthFirstSearch(root)(4) == Some(4))
-    assert(BinaryTree.depthFirstSearch(root)(2) == Some(2))
-    assert(BinaryTree.depthFirstSearch(root)(3) == Some(3))
-    assert(BinaryTree.depthFirstSearch(root)(5) == Some(5))
-    assert(BinaryTree.depthFirstSearch(root)(1) == Some(1))
+  test("dfs - 検索対象が含まれていれば値と検索計算量を返す") {
+    assert(BinaryTree.depthFirstSearch(root)(4) == Found(4, 1))
+    assert(BinaryTree.depthFirstSearch(root)(2) == Found(2, 2))
+    assert(BinaryTree.depthFirstSearch(root)(3) == Found(3, 4))
+    assert(BinaryTree.depthFirstSearch(root)(5) == Found(5, 3))
+    assert(BinaryTree.depthFirstSearch(root)(1) == Found(1, 5))
   }
 
   test("dfs - 検索対象が含まれていなければ値を返さない") {
-    assert(BinaryTree.depthFirstSearch(root)(6) == None)
-    assert(BinaryTree.depthFirstSearch(root)(0) == None)
+    assert(BinaryTree.depthFirstSearch(root)(6) == NotFound(5))
+    assert(BinaryTree.depthFirstSearch(root)(0) == NotFound(5))
   }
 
 }
