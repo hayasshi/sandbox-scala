f69ff56bc8a681a2877e1733c2890ebeec05551c
hayasshi <daisuke0884@gmail.com>
2019-09-22 15:33:46 +0900

    [parser] 不要な P() を削除

--- parser/src/main/scala/memcached/MemcachedParser.scala
+++ parser/src/main/scala/memcached/MemcachedParser.scala
@@ -4,12 +4,12 @@ import fastparse._, NoWhitespace._
 
 object MemcachedParser {
 
-  def tokenSeparator[_: P]: P[Unit] = P(CharPred(_.isSpaceChar)).rep(1)
+  def tokenSeparator[_: P]: P[Unit] = CharPred(_.isSpaceChar).rep(1)
 
   def lineSeparator[_: P]: P[Unit] = P("\r\n")
 
-  def key[_: P]: P[String] = P(CharPred(c => !c.isControl && !c.isSpaceChar)).rep.!.filter(!_.contains("\r\n"))
+  def key[_: P]: P[String] = CharPred(c => !c.isControl && !c.isSpaceChar).rep.!.filter(!_.contains("\r\n"))
 
-  def GetParser[_: P]: P[Seq[String]] = P(Start ~ ("get" ~ "s".?)) ~ (tokenSeparator ~ key).rep ~ lineSeparator ~ End
+  def GetParser[_: P]: P[Seq[String]] = Start ~ ("get" ~ "s".?) ~ (tokenSeparator ~ key).rep ~ lineSeparator ~ End
 
 }
